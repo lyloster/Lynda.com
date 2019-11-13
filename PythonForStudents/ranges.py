99bbc7368b24cb28b2031b31b5148991ac744fb6
Kristina Ilyovska <lyloster@gmail.com>
2019-11-13 18:29:39 -0500

    Ranges notes

---
+++ PythonForStudents/ranges.py
@@ -0,0 +1,12 @@
+for i in [1,2,3,4,5]:
+	print(i)
+
+#range excluding the last element
+for i in range(1,11):
+	print(i)
+
+#prints 
+print(range(1,11))
+
+#converts the elements in the range into a list
+print(list(range(1,11)))
