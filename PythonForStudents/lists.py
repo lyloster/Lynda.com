dab53a53dc9745c1687008ea82fe82e2e5d24db9
Kristina Ilyovska <lyloster@gmail.com>
2019-11-13 18:11:21 -0500

    Create lists

---
+++ PythonForStudents/lists.py
@@ -0,0 +1,41 @@
+numbers = [1,2,3]
+pizza_toppings = ["cheese", "pepperoni"]
+mixed_list = [1, "cheese", 2, "pepperoni"]
+list_of_lists = [[1,2], [3,4]]
+
+#access individual element
+print(pizza_toppings[0])
+
+#from the end
+print(pizza_toppings[-1])
+
+#in python - out of bound errors
+#lists are mutable
+
+#length of list
+print(len(pizza_toppings))
+
+#at the end of the list
+pizza_toppings.append("pineapple")
+
+#at specific index
+pizza_toppings.insert(0, "mushrooms")
+
+#remove
+del pizza_toppings[0]
+
+#remove from the end of the list
+pizza_toppings.pop()
+
+#remove specific element, removes the first instance
+pizza_toppings.remove("cheese")
+
+print(pizza_toppings)
+
+pizza_toppings = ["cheese", "cheese", "pepperoni"]
+pizza_toppings.remove("cheese")
+
+print(pizza_toppings)
+
+
+
